b4da00f87213c62d9c63ab7df563e74d6c3112cf
emesc <em@curlycoder.me>
2017-06-10 21:11:35 +1000

    Checked if json file is loaded during initialization

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,19 +1,23 @@
 require 'json'
 
 class Scrabble
-  class << self
-    def letter_values
-      data_path = File.join(File.dirname(__FILE__), "..", "data", "letter_values.json")
-      letter_values = File.read(data_path)
-      JSON.load(letter_values)
-    end
+  def self.letter_values
+    data_path = File.join(File.dirname(__FILE__), "..", "data", "letter_values.json")
+    letter_values = File.read(data_path)
+    JSON.load(letter_values)
   end
 
   def initialize
+    if loaded?(self.class.letter_values)
+      puts "Letter values found"
+    else
+      puts "Letter values not found"
+      exit!
+    end
   end
 
   def score(word)
-    valid?(word) ? word.strip.chars.map { |char| value(char) }.reduce(:+) : 0
+    valid?(word) ? word.chars.map { |char| value(char) }.reduce(:+) : 0
   end
 
   private
@@ -23,5 +27,9 @@ class Scrabble
 
     def value(char)
       self.class.letter_values[char.upcase]
+    end
+
+    def loaded?(file)
+      !!file
     end
 end
