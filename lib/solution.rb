e6109366c1b4b99dd0667ed06816b9a686632bcd
emesc <em@curlycoder.me>
2017-06-11 01:01:10 +1000

    Moved getting user input to a separate method

--- lib/solution.rb
+++ lib/solution.rb
@@ -2,16 +2,19 @@ class Solution
   class << self
     def scrabble
       game = Scrabble.new
-      print "Enter a word to score > "
-      word = gets.chomp.downcase.strip
+      word = get_command("word", "score")
       puts "Score is: #{game.score(word)}"
     end
 
     def credit
-      print "Enter a number to validate > "
-      account = gets.chomp.downcase.strip
+      account = get_command("number", "validate")
       credit_check = CreditCheck.new(account)
       puts "#{credit_check.valid?}" if credit_check.integer? account
+    end
+
+    def get_command(input, action)
+      print "Enter a #{input} to #{action} > "
+      gets.chomp.downcase.strip
     end
   end
 end
