afd8a0068927e5e2df50bc7f4403459dee28fe86
emesc <em@curlycoder.me>
2017-06-12 12:22:33 +1000

    Loaded letter values only once at the top level

--- bin/guide.rb
+++ bin/guide.rb
@@ -2,16 +2,18 @@
 
 Dir[File.expand_path("./lib/**/*.rb")].each { |f| require f }
 COMMANDS = %w(credit scrabble)
+game = Scrabble.new
 
 loop do
   puts  "\nCommands: #{COMMANDS.join(" or ")} (quit to exit)"
+  print "> "
   input = gets.chomp.downcase.strip
 
   if input == "quit"
     puts "\n<<< Thank you and goodbye :-) >>>\n\n"
     break
   elsif COMMANDS.include? input
-    Solution.send input
+    input == COMMANDS.last ? Solution.send(input, game) : Solution.send(input)
   else
     puts "\nI don't understand that command"
   end
